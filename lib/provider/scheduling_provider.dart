700ea64ddb2e1c47e257cd086e8174421e963755
Hani Elsari <44626848+hanielsari@users.noreply.github.com>
2022-05-09 20:50:55 +0700

    Update scheduling_provider.dart

--- lib/provider/scheduling_provider.dart
+++ lib/provider/scheduling_provider.dart
@@ -2,12 +2,29 @@ import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:submissiondicoding/utils/background_service.dart';
 import 'package:submissiondicoding/utils/date_time_helper.dart';
-
+import 'package:submissiondicoding/data/preferences_helper.dart';
 
 class SchedulingProvider extends ChangeNotifier {
-  bool _isScheduled = false;
+  PreferencesHelper preferencesHelper;
+
+  SchedulingProvider({required this.preferencesHelper}) {
+     _getDailyRestaurantPreference();
+  }
+
+  bool _isScheduled = true;
 
   bool get isScheduled => _isScheduled;
+
+  void _getDailyRestaurantPreference() async {
+    _isScheduled = await preferencesHelper.isDailyRestaurantActive;
+    notifyListeners();
+  }
+
+  void enableDailyRestaurant(bool value) {
+    preferencesHelper.setDailyRestaurant(value);
+    scheduledRestaurant(value);
+    _getDailyRestaurantPreference();
+  }
 
   Future<bool> scheduledRestaurant(bool value) async {
     _isScheduled = value;
@@ -15,7 +32,7 @@ class SchedulingProvider extends ChangeNotifier {
       print('Scheduling Activated');
       notifyListeners();
       return await AndroidAlarmManager.periodic(
-          Duration(hours: 0), 1, BackgroundService.callback,
+          Duration(hours: 24), 1, BackgroundService.callback,
           startAt: DateTimeHelper.format(), exact: true, wakeup: true);
     } else {
       print('Scheduling Canceled');
